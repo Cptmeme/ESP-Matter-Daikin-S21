15780956a9e882dd6c1fc921412765d4899a0342
Cptmeme <61885007+Cptmeme@users.noreply.github.com>
2026-05-10 17:36:30 +0200

    change name/version v2

---
+++ Thermostat_Daikin/main/CHIPProjectConfig.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// This changes the "Manufacturer" field in the device settings
+#define CHIP_DEVICE_CONFIG_DEVICE_VENDOR_NAME            "Daikin"
+
+// This changes the "Model" field and the default name in the app
+#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_NAME           "Daikin A/C"
+
+// This is the "hidden" label that some apps use as the primary display name
+#define CHIP_DEVICE_CONFIG_DEFAULT_NODE_LABEL            "Daikin Thermostat"
